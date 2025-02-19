2fb5dd33c1a8bceb8048d893c83bcab175b7a657
silu80 <mylaptop8018@gmail.com>
2025-02-19 16:09:05 +0530

    Adding master branch

---
+++ src/main/java/com/example/dio/DineInOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineInOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineInOrderApiApplication.class, args);
+	}
+
+}
